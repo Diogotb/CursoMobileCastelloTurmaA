d1b0eecfba7e2ba92738a99a0e7715763c62a50a
Diogotb <digogo@gmail.com>
2025-10-01 12:56:21 -0300

    01/10

--- FireBase/cine_favorite/lib/main.dart
+++ FireBase/cine_favorite/lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:cine_favorite/views/favorite_view.dart';
+import 'package:cine_favorite/views/login_view.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
